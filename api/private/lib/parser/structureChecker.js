941e65c13fd0165e96fc872a71483debc808a18c
ltvantil <ltvantil@ncsu.edu>
2021-03-20 18:22:30 -0400

    Fixed strucuture checker

--- api/private/lib/parser/structureChecker.js
+++ api/private/lib/parser/structureChecker.js
@@ -2,8 +2,16 @@ function evaluateStructure(resumeObj, output) {
     if (resumeObj.getName()) {
         evaluateStructure.name = resumeObj.getName();
     } else {
-        evaluateStructure.name = "No name found.";
+        evaluateStructure.name = "";
     }
+
+    if (resumeObj.getEmail()) {
+        evaluateStructure.email = resumeObj.getEmail();
+    } else {
+        evaluateStructure.email = "No name found.";
+    }
+
+
 }
 
 module.exports = evaluateStructure;
