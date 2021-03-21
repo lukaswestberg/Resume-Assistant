1af1b826491bca2491e7e496a56158a3c9bb93cc
hakon <hakon@ncsu.edu>
2021-03-21 11:06:56 -0400

    File upload form component
    - not coded to do anything yet but the uploader now exists and can be used in other functions
    
    Signed-off-by: hakon <hakon@ncsu.edu>

--- client/src/components/TitleBar.js
+++ client/src/components/TitleBar.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '../App.css';
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ResumeUploadModal from "./ResumeUploadModal";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,16 +25,16 @@ export default function TitleBar() {
     const classes = useStyles();
     return (
         <AppBar position={"static"}>
-            <Toolbar>
-                <IconButton edge={"start"} className={classes.menuButton} color={"inherit"} aria-label={"menu"}>
+            <Toolbar variant={"dense"}>
+               {/* <IconButton edge={"start"} className={classes.menuButton} color={"inherit"} aria-label={"menu"}>
                     <MenuIcon/>
-                </IconButton>
+                </IconButton>*/}
                 <Typography variant={"h6"} className={classes.title}>
-                    Sex
+                    Resume Assistant
                 </Typography>
-                <Button color={"inherit"}>
+                {/*<Button color={"inherit"}>
                     Upload Resume
-                </Button>
+                </Button>*/}
             </Toolbar>
         </AppBar>
     )
